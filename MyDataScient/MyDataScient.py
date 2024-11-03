665a82079912bffdeaa03908858614b1ea543f9c
User <User@DESKTOP-S4GLRA5>
2024-11-03 18:56:04 +0800

    Добавьте файлы проекта.

---
+++ MyDataScient/MyDataScient.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import scipy.stats as stats
+data=pd.read_csv('C:\\Users\\User\\source\\repos\\MyDataScient\\possum.csv')
+print(data)
